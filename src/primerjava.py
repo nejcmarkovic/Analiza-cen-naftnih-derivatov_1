219c23006ebf4678b495bc51b7c8d82561cce688
nejcmarkovic <94306744+nejcmarkovic@users.noreply.github.com>
2026-08-23 21:13:14 +0200

    Popravek

--- src/primerjava.py
+++ src/primerjava.py
@@ -3,16 +3,15 @@ import pandas as pd
 
 
 def povprecne_cene(tabela, stolpci):
-    """Vrne povprecno ceno za vsak podan stolpec, urejeno od najnizje do najvisje."""
+    
     return tabela[stolpci].mean().sort_values()
 
 
 def razlika_do_referencne(tabela, stolpci, referencni_stolpec):
-    """
-    Za vsak stolpec iz seznama izracuna razliko do referencnega stolpca
-    (npr. za vsako drzavo, koliko je drazja/cenejsa od Slovenije).
-    Vrne nov DataFrame z datumom in stolpci razlik.
-    """
+
+
+
+   
     razlike = pd.DataFrame()
     razlike["datum"] = tabela["datum"]
 
@@ -26,15 +25,14 @@ def razlika_do_referencne(tabela, stolpci, referencni_stolpec):
 
 
 def korelacijska_matrika(tabela, stolpci):
-    """Vrne korelacijsko matriko med podanimi stolpci (drzavami)."""
+    
     return tabela[stolpci].corr()
 
 
 def najvecja_razlika(tabela, stolpec_a, stolpec_b):
-    """
-    Poisce datum, ko je bila razlika med dvema stolpcema (drzavama)
-    najvecja, in vrne ta datum ter velikost razlike.
-    """
+
+
+    
     razlike = (tabela[stolpec_a] - tabela[stolpec_b]).abs()
     indeks_najvecje = razlike.idxmax()
     return tabela.loc[indeks_najvecje, "datum"], razlike[indeks_najvecje]
